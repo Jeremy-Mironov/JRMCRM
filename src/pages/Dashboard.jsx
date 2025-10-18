40bcac17e9fe7a5166504e3fafcca45f87a581a3
Jeremy-Mironov@ <@jeremymironov@gmail.com>
2025-10-18 15:32:30 -0700

    Add Vercel rewrites for SPA

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 
 export default function Dashboard() {
     return (
-        <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 bg-blue-600 text-white px-4 py-2 rounded-lg shadow-lg">
+        <div className="">
             <h1 className="text-right  font-bold text-gray-800 dark:text-white mb-6 ">
                 Dashboard
             </h1>
